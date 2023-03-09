4ba7f72f5d3bf85fb5c36566e80124c84b68bbc0
Maryna Dzuh <maryna.dzuh@gmail.com>
2023-03-10 01:58:22 +0200

    fix

--- src/components/AppBar/AppBar.styled.jsx
+++ src/components/AppBar/AppBar.styled.jsx
@@ -1,12 +1,18 @@
 import styled from '@emotion/styled'
 export const HeaderSt = styled.header`
+@media screen and (max-width: 424px) {
+     background-color:#c0c0c0ba;
+     margin: 0;
+}
 @media screen and (max-width: 767px) {
-     padding: 10px 20px;
+     padding: 10px 10px;
+     
 }
     display: flex;
+    align-items: center;
     justify-content: space-between;
-    padding: 10px 60px;
+    padding: 20px 60px;
     border-bottom: 1px solid black;
     min-width: 320px;
-    margin: 10px auto;
+    margin: 0 auto;
 `
--- src/components/UserMenu/UserMenu.styled.jsx
+++ src/components/UserMenu/UserMenu.styled.jsx
@@ -1,6 +1,11 @@
 import styled from '@emotion/styled'
 
 export const Menu = styled.div`
+@media screen and (max-width: 424px) {
+     display: block;
+     
+     text-align:right;
+}
     display: flex;
     align-items: center;
     gap: 10px;
@@ -11,10 +16,15 @@ export const Text = styled.div`
     margin: 0;
 `
 export const Butt = styled.div`
+@media screen and (max-width: 424px) {
+     margin-top:5px;
+     margin-left: auto;
+}
     /* background-color: #ffa600a7; */
      border: 1px solid orange;
     margin: 0;
     padding: 5px 5px;
+    max-width:70px;
    
     border-radius: 8px;
     cursor: pointer;
